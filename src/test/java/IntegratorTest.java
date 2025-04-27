8accb84fa629d72941d7cadb53ef453fb991c083
Ulyana <uliana.malakhova@yandex.ru>
2025-04-27 21:54:23 +0700

    Создание теста для интегрирования функции x*x

--- src/test/java/IntegratorTest.java
+++ src/test/java/IntegratorTest.java
@@ -2,10 +2,17 @@ import org.example.Integrator;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class IntegratorTest {
     @Test
     void integratorClassCreationTest(){
         Integrator integrator = new Integrator();
         Assertions.assertNotNull(integrator);
+    }
+    @Test
+    public void testIntegrationSqrtX(){
+        Integrator integrator = new Integrator();
+        assertEquals(1.0 / 3.0, integrator.integrate(x -> x * x, 0, 1, 1000), 0.01);
     }
 }
