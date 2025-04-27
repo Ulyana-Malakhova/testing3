42bcf345e818973b90ebaebcf12df5b60ee16b09
Ulyana <uliana.malakhova@yandex.ru>
2025-04-27 21:56:55 +0700

    Заглушка для интегрирования функции x*x

--- src/main/java/org/example/Integrator.java
+++ src/main/java/org/example/Integrator.java
@@ -1,4 +1,11 @@
 package org.example;
 
+
+import java.util.function.Function;
+
 public class Integrator {
+    public double integrate(Function<Double, Double> function, double a, double b, int n) {
+        //todo реализовать интегрирование функции
+        return 1.0 / 3.0;
+    }
 }
