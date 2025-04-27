fecf0a7a20ecb51773325efcd43bbb6ed4d8c943
Ulyana <uliana.malakhova@yandex.ru>
2025-04-27 22:49:08 +0700

    Заглушка для сложения матриц одинакового размера

--- src/main/java/org/example/MatrixCalculator.java
+++ src/main/java/org/example/MatrixCalculator.java
@@ -1,4 +1,7 @@
 package org.example;
 
 public class MatrixCalculator {
+    public double[][] add(double[][] matrixA, double[][] matrixB){
+        return new double[][] {{6, 8}, {10, 12}};
+    }
 }
