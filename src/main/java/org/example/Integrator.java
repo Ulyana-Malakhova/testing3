54dcb797ac29ec147bde2081ef5570fc83693b76
Ulyana <uliana.malakhova@yandex.ru>
2025-04-28 00:43:43 +0700

    Реализация решения интегрирования функций

--- src/main/java/org/example/Integrator.java
+++ src/main/java/org/example/Integrator.java
@@ -13,14 +13,14 @@ public class Integrator {
      * @return результат интегрирования
      */
     public double integrate(Function<Double, Double> function, double a, double b, int n) {
-        //todo реализовать интегрирование функции
-        if (function.apply(0.5).equals(0.25) && a == 0 && b == 1 && n == 1000)
-            return 1.0 / 3.0;
-        else if (Math.abs(function.apply(0.0) - 0.0) < 1e-9 &&
-                Math.abs(function.apply(Math.PI / 2) - 1.0) < 1e-9 &&
-                Math.abs(function.apply(Math.PI) - 0.0) < 1e-9
-                && a== 0 && b == Math.PI && n== 1000)
-            return 2.0;
-        throw new IllegalArgumentException("Интегрирование работает только для x*x и sin(x)");
+        double step = (b - a) / n;
+        double sum = 0.0;
+
+        for (int i = 0; i < n; i++) {
+            double x = a + i * step;
+            sum += function.apply(x);
+        }
+
+        return sum * step;
     }
 }
