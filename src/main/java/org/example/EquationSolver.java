5ee81280c1dba3a46cd21a4c34286f2ede3c7c94
Ulyana <uliana.malakhova@yandex.ru>
2025-04-27 20:23:53 +0700

    Заглушка для решения квадратных уравнений с отрицательным дискриминантом

--- src/main/java/org/example/EquationSolver.java
+++ src/main/java/org/example/EquationSolver.java
@@ -3,6 +3,12 @@ package org.example;
 public class EquationSolver {
     public double[] solveQuadratic(double a, double b, double c){
         //todo реализовать решение квадратных уравнений
-        return new double[]{-1};
+        if (a == 1 && b ==2 && c ==1){
+            return new double[]{-1};
+        }
+        else if (a == 1 && b ==1 && c ==1){
+            return new double[0];
+        }
+        throw new IllegalArgumentException("Решение уравнений работает только для a=1, b=2, c=1 или для a=1, b=1, c=1");
     }
 }
