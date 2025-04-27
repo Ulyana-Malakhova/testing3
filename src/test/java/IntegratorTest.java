01f2c097a08bf25d31c081ada1d34b574757d2c0
Ulyana <uliana.malakhova@yandex.ru>
2025-04-27 22:16:08 +0700

    Создание теста для интегрирования функции sin x

--- src/test/java/IntegratorTest.java
+++ src/test/java/IntegratorTest.java
@@ -15,4 +15,9 @@ public class IntegratorTest {
         Integrator integrator = new Integrator();
         assertEquals(1.0 / 3.0, integrator.integrate(x -> x * x, 0, 1, 1000), 0.01);
     }
+    @Test
+    public void testIntegrationSinX(){
+        Integrator integrator = new Integrator();
+        assertEquals(2.0, integrator.integrate(x -> Math.sin(x), 0, Math.PI, 1000), 0.01);
+    }
 }
