3a5a9d32e7343d0189bdb9e8daafff43466ed375
Ulyana <uliana.malakhova@yandex.ru>
2025-04-27 21:46:46 +0700

    Тест создания класса интегрирования

---
+++ src/test/java/IntegratorTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class IntegratorTest {
+    @Test
+    void integratorClassCreationTest(){
+        Integrator integrator = new Integrator();
+        Assertions.assertNotNull(integrator);
+    }
+}
