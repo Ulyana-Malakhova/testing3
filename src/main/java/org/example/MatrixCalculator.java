01187e057e0048248a146a748516cabe90e4375c
Ulyana <uliana.malakhova@yandex.ru>
2025-04-27 22:52:24 +0700

    Заглушка для сложения матриц разного размера

--- src/main/java/org/example/MatrixCalculator.java
+++ src/main/java/org/example/MatrixCalculator.java
@@ -2,6 +2,9 @@ package org.example;
 
 public class MatrixCalculator {
     public double[][] add(double[][] matrixA, double[][] matrixB){
+        if (matrixA.length != matrixB.length || matrixA[0].length != matrixB[0].length) {
+            throw new IllegalArgumentException("Матрицы должны иметь одинаковые размеры.");
+        }
         return new double[][] {{6, 8}, {10, 12}};
     }
 }
