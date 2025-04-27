35cd10125cbe24be2df15c7a9e57842b6385d185
Ulyana <uliana.malakhova@yandex.ru>
2025-04-28 00:41:42 +0700

    Реализация решения квадратных уравнений

--- src/main/java/org/example/EquationSolver.java
+++ src/main/java/org/example/EquationSolver.java
@@ -9,16 +9,15 @@ public class EquationSolver {
      * @return решение
      */
     public double[] solveQuadratic(double a, double b, double c){
-        //todo реализовать решение квадратных уравнений
-        if (a == 1 && b ==2 && c ==1){
-            return new double[]{-1};
+        double discriminant = b * b - 4 * a * c;
+        if (discriminant < 0) {
+            return new double[0]; // Нет действительных корней
+        } else if (discriminant == 0) {
+            return new double[]{-b / (2 * a)}; // Один корень
+        } else {
+            double root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+            double root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+            return new double[]{root1, root2}; // Два корня
         }
-        else if (a == 1 && b ==1 && c ==1){
-            return new double[0];
-        }
-        else if (a == 2 && b == -3 && c == -2){
-            return new double[]{2, -0.5};
-        }
-        throw new IllegalArgumentException("Решение уравнений работает только для a=1, b=2, c=1 или для a=1, b=1, c=1 или для a=2, b=-3, c=-2");
     }
 }
