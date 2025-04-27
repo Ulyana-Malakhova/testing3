ec8b6e22aacc16a3a3ec74b434ef0a7a0248ac3c
Ulyana <uliana.malakhova@yandex.ru>
2025-04-27 20:04:28 +0700

    Заглушка для решения квадратных уравнений

--- src/main/java/org/example/EquationSolver.java
+++ src/main/java/org/example/EquationSolver.java
@@ -1,4 +1,8 @@
 package org.example;
 
 public class EquationSolver {
+    public double[] solveQuadratic(double a, double b, double c){
+        //todo реализовать решение квадратных уравнений
+        return new double[]{-1};
+    }
 }
