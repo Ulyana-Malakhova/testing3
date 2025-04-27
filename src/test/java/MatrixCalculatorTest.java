01b615c7305fa9976009b4f77bdd967a06c794ee
Ulyana <uliana.malakhova@yandex.ru>
2025-04-27 23:30:20 +0700

    Тест умножения матриц

--- src/test/java/MatrixCalculatorTest.java
+++ src/test/java/MatrixCalculatorTest.java
@@ -55,4 +55,15 @@ public class MatrixCalculatorTest {
         double[][] result = calculator.subtract(matrixA, matrixB);
         assertArrayEquals(expected, result);
     }
+
+    @Test
+    public void testMatrixMultiplication() {
+        MatrixCalculator calculator = new MatrixCalculator();
+        double[][] matrixA = {{1, 2}, {3, 4}};
+        double[][] matrixB = {{5, 6}, {7, 8}};
+        double[][] expected = {{19, 22}, {43, 50}};
+
+        double[][] result = calculator.multiply(matrixA, matrixB);
+        assertArrayEquals(expected, result);
+    }
 }
